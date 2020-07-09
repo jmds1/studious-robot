70f3398215823fd7f2f39fdb0408ea36c43d409a
Jesus <jesusmartinez939@gmail.com>
2020-07-09 12:18:33 -0700

    Working with basic Jinja2 templates

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -3,9 +3,18 @@ import flask
 app = flask.Flask(__name__)
 
 
+def get_latest_package():
+    return [
+        {'name': 'flask', 'version': '1.2.3'},
+        {'name': 'sqlalchemy', 'version': '2.2.0'},
+        {'name': 'passlib', 'version': '3.0.0'},
+    ]
+
+
 @app.route('/')
 def index():
-    return "Hello World!"
+    test_packages = get_latest_package()
+    return flask.render_template('index.html', packages=test_packages)
 
 
 if __name__ == '__main__':
